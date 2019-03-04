7fb5e0bbf696ef9dc744b307fc0a3b16ce1bf035
Maxim Rakhmanin <rakhmaxdev@gmail.com>
2019-03-04 20:31:45 +0300

    Improve

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,52 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-// const babel = require('gulp-babel');
-const htmlmin = require('gulp-htmlmin');
-const cssmin = require('gulp-cssmin');
-const concat = require('gulp-concat');
-const sourcemaps = require('gulp-sourcemaps');
-const gulpIf = require('gulp-if');
-const htmlPartial = require('gulp-html-partial');
+const gulp          = require('gulp');
+const gulpIf        = require('gulp-if');
+const browserSync   = require('browser-sync').create();
+const sass          = require('gulp-sass');
+const htmlmin       = require('gulp-htmlmin');
+const cssmin        = require('gulp-cssmin');
+const uglify        = require('gulp-uglify');
+const imagemin      = require('gulp-imagemin');
+const concat        = require('gulp-concat');
+const jsImport      = require('gulp-js-import');
+const sourcemaps    = require('gulp-sourcemaps');
+const htmlPartial   = require('gulp-html-partial');
 
 const isProd = process.env.NODE_ENV === 'prod';
 
 function html() {
     return gulp.src('src/index.html')
-        .pipe(htmlPartial({ basePath: 'src/components/' }))
+        .pipe(htmlPartial({ basePath: 'src/partials/' }))
         .pipe(gulpIf(isProd, htmlmin({ collapseWhitespace: true })))
         .pipe(gulp.dest('dist'));  
 }
 
 function css() {
-    return gulp.src('src/assets/sass/style.scss')
+    return gulp.src('src/sass/style.scss')
         .pipe(gulpIf(!isProd, sourcemaps.init()))
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
         .pipe(gulpIf(!isProd, sourcemaps.write()))
         .pipe(gulpIf(isProd, cssmin()))
         .pipe(gulp.dest('dist/css/'));
 }
 
 function js() {
-    return gulp.src('src/components/*/*.js')
-        // .pipe(babel({
-        //     presets: ['@babel/env']
-        // }))
+    return gulp.src('src/js/*.js')
+        .pipe(jsImport({hideConsole: true}))
         .pipe(concat('all.js'))
+        .pipe(gulpIf(isProd, uglify()))
         .pipe(gulp.dest('dist/js'));
+}
+
+function img() {
+    return gulp.src('src/img/*')
+        .pipe(gulpIf(isProd, imagemin()))
+        .pipe(gulp.dest('dist/img/'));
 }
 
 function serve() {
     browserSync.init({
         open: false,
-        server: './dist',
-        port: 3000
+        server: './dist'
     });
 }
 
@@ -53,6 +59,7 @@ function watchFiles() {
     gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
     gulp.watch('src/**/*.scss', gulp.series(css, browserSyncReload));
     gulp.watch('src/**/*.js', gulp.series(js, browserSyncReload));
+    gulp.watch('src/assets/img/**/*.*', gulp.series(img));
 
     return;
 }
@@ -60,5 +67,5 @@ function watchFiles() {
 exports.css = css;
 exports.html = html;
 exports.js = js;
-exports.serve = gulp.parallel(html, css, js, watchFiles, serve);
-exports.default = gulp.parallel(html, css, js);
+exports.serve = gulp.parallel(html, css, js, img, watchFiles, serve);
+exports.default = gulp.parallel(html, css, js, img);
--- src/assets/js/index.js
+++
@@ -1,3 +0,0 @@
-const foo = () => {
-    alert('bar');
-}
